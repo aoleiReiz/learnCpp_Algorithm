003fa9a3afcf6ff741c42c019a9679e31d9a69a5
Bao <aolei.bao@accenture.com>
2023-01-17 18:15:38 +0800

    mid algoexpert

--- algoexpert/mid/MidLast8.cpp
+++ algoexpert/mid/MidLast8.cpp
@@ -33,3 +33,40 @@ bool MidLast8::oneEdit(string stringOne, string stringTwo) {
     }
     return true;
 }
+
+bool MidLast8::symmetricalTree(BinaryTree *tree) {
+    return symmetricalTreeHelper(tree, tree);
+}
+
+bool MidLast8::symmetricalTreeHelper(BinaryTree *tree1, BinaryTree *tree2) {
+    if (tree1 == nullptr && tree2 == nullptr){
+        return true;
+    }
+    else if(tree1 && tree2){
+        return tree2->value == tree1->value && symmetricalTreeHelper(tree1->left, tree2->right) &&
+                symmetricalTreeHelper(tree1->right, tree2->left);
+    }
+    return false;
+}
+
+LinkedList *MidLast8::mergingLinkedLists(LinkedList *linkedListOne, LinkedList *linkedListTwo) {
+    unordered_set<LinkedList*>setOne;
+    unordered_set<LinkedList*>setTwo;
+    LinkedList *ptrOne = linkedListOne;
+    LinkedList *ptrTwo = linkedListTwo;
+    while (ptrOne && ptrTwo){
+        auto it1 = setTwo.find(ptrOne);
+        if (it1 != setTwo.end()){
+            return *it1;
+        }
+        auto it2 = setOne.find(ptrTwo);
+        if (it2 != setOne.end()){
+            return *it2;
+        }
+        setOne.insert(ptrOne);
+        setTwo.insert(ptrTwo);
+        ptrOne ++;
+        ptrTwo ++;
+    }
+    return nullptr;
+}
--- algoexpert/mid/MidLast8.h
+++ algoexpert/mid/MidLast8.h
@@ -5,12 +5,41 @@
 #ifndef LEARNCPP_ALGORITHM_MIDLAST8_H
 #define LEARNCPP_ALGORITHM_MIDLAST8_H
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
+
+
+class BinaryTree {
+public:
+    int value;
+    BinaryTree *left = nullptr;
+    BinaryTree *right = nullptr;
+
+    BinaryTree(int value) {
+        this->value = value;
+    }
+};
+
+
+class LinkedList {
+public:
+    int value;
+    LinkedList *next = nullptr;
+
+    LinkedList(int value) {
+        this->value = value;
+    }
+};
+
 
 class MidLast8 {
 public:
     bool oneEdit(string stringOne, string stringTwo);
+    bool symmetricalTree(BinaryTree* tree);
+    bool symmetricalTreeHelper(BinaryTree* tree1, BinaryTree* tree2);
+    LinkedList* mergingLinkedLists(LinkedList* linkedListOne, LinkedList* linkedListTwo);
 };
 
 
