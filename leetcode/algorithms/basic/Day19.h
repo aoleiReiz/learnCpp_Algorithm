8b4c5c343abe4740c69f6efe70d7edb56c3fad7f
Bao <aolei.bao@accenture.com>
2023-02-01 10:26:15 +0800

    day19

--- leetcode/algorithms/basic/Day19.h
+++ leetcode/algorithms/basic/Day19.h
@@ -33,4 +33,157 @@ string decodeMessage(string key, string message) {
     return ret;
 }
 
+class AuthenticationManager {
+private:
+    unordered_map<string, int> auth;
+    int t;
+public:
+    AuthenticationManager(int timeToLive) {
+        t = timeToLive;
+    }
+
+    void generate(string tokenId, int currentTime) {
+        auth[tokenId] = currentTime + t;
+    }
+
+    void renew(string tokenId, int currentTime) {
+        auto iter = auth.find(tokenId);
+        if (iter != auth.end() && iter->second > currentTime){
+            iter->second = currentTime + t;
+        }
+    }
+
+    int countUnexpiredTokens(int currentTime) {
+        int count = 0;
+        for (auto iter: auth) {
+            if(iter.second > currentTime){
+                count ++;
+            }
+        }
+        return count;
+    }
+};
+
+
+struct Node{
+    int val;
+    Node * next;
+    Node(int v):val(v), next(nullptr){}
+};
+
+class MyLinkedList {
+private:
+    Node * head;
+    Node * tail;
+    int size;
+public:
+    MyLinkedList() {
+        head = nullptr;
+        tail = nullptr;
+        size = 0;
+    }
+
+    int get(int index) {
+        if (index < 0 || index >= size || size == 0){
+            return -1;
+        }
+        int count = 0;
+        Node * p = head;
+        while (count < index){
+            p = p->next;
+            count ++;
+        }
+        return p->val;
+    }
+
+    void addAtHead(int val) {
+        if (head == nullptr){
+            head = new Node(val);
+            tail = head;
+        }else{
+            Node *p = new Node(val);
+            p->next = head;
+            head = p;
+        }
+        size++;
+    }
+
+    void addAtTail(int val) {
+        if (tail == nullptr){
+            tail = new Node(val);
+            head = tail;
+        }else{
+            tail->next = new Node(val);
+            tail = tail->next;
+        }
+        size ++;
+    }
+
+    void addAtIndex(int index, int val) {
+        if (index > size){
+            return;
+        }
+        if (index <= 0){
+            addAtHead(val);
+        }else if(index == size){
+            addAtTail(val);
+        }else{
+            Node * prev = head;
+            int count = 0;
+            while (count < index - 1){
+                prev = prev->next;
+                count ++;
+            }
+            Node * temp = prev->next;
+            prev->next = new Node(val);
+            prev->next->next = temp;
+            size ++ ;
+        }
+    }
+
+    void deleteAtIndex(int index) {
+        if (index < 0 || index >= size){
+            return;
+        }
+        Node * dummyHead = new Node(-1);
+        dummyHead -> next = head;
+        int count = 0;
+        Node * prev = dummyHead;
+        while (count < index){
+            prev = prev->next;
+            count ++;
+        }
+        Node * temp = prev->next;
+        prev->next = temp -> next;
+        head = dummyHead->next;
+        if (index == size - 1){
+            tail = prev;
+        }
+        size --;
+    }
+
+    void printList(){
+        cout << size << endl;
+        Node *p = head;
+        while (p){
+            cout << p->val << " -> ";
+            p = p->next;
+        }
+        cout << endl;
+    }
+};
+
+int rangeBitwiseAnd(int left, int right) {
+    int shift = 0;
+    // 找到公共前缀
+    while (left < right) {
+        left >>= 1;
+        right >>= 1;
+        ++shift;
+    }
+    return left << shift;
+}
+
+
+
 #endif //LEARNCPP_ALGORITHM_DAY19_H
--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 
-#include "leetcode/algorithms/basic/Day13.h"
+#include "leetcode/algorithms/basic/Day19.h"
 
 using namespace std;
 
 int main() {
+    MyLinkedList li;
+    li.addAtHead(0);
+    li.printList();
+    li.addAtIndex(1, 4);
+    li.printList();
+    li.addAtTail(8);
+    li.printList();
+    li.addAtHead(5);
+    li.printList();
+    li.addAtIndex(4, 3);
+    li.printList();
+    li.addAtTail(0);
+    li.printList();
+    li.addAtTail(5);
+    li.printList();
+    li.addAtIndex(6, 3);
+    li.printList();
+    li.deleteAtIndex(7);
+    li.printList();
+    li.deleteAtIndex(5);
+    li.printList();
+    li.addAtTail(4);
+    li.printList();
+    cout << li.get(1)<<endl;
+    li.deleteAtIndex(0);
+    cout << li.get(0)<<endl;
 
-    cout <<uniquePaths(3,7)<<endl;
     return 0;
 }
