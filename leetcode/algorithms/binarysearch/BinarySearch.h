38a9929e6f9b0ad8c0b08437a94daf99dc67647d
aoleiReiz <baoaolei1989@outlook.com>
2023-03-07 06:26:39 +0800

    binary search

---
+++ leetcode/algorithms/binarysearch/BinarySearch.h
@@ -0,0 +1,19 @@
+//
+// Created by 包傲磊 on 2023/3/7.
+//
+
+#ifndef LEARNCPP_ALGORITHM_BINARYSEARCH_H
+#define LEARNCPP_ALGORITHM_BINARYSEARCH_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class BinarySearch {
+public:
+    int search(vector<int> &nums, int target);
+};
+
+
+#endif //LEARNCPP_ALGORITHM_BINARYSEARCH_H
