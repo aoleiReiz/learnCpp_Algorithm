80c087ca4a9b9c7eeac4ccfa1e978ba608f7b1f2
aoleiReiz <baoaolei1989@outlook.com>
2023-03-07 05:57:41 +0800

    commit from mac

--- leetcode/daily/Solution2.h
+++ leetcode/daily/Solution2.h
@@ -9,6 +9,7 @@
 #include <string>
 #include <unordered_map>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -62,5 +63,29 @@ public:
         return ans;
     }
 
+    string alphabetBoardPath(string target) {
+            int cx = 0, cy = 0;
+            string res;
+            for (char c : target) {
+                int nx = (c - 'a') / 5;
+                int ny = (c - 'a') % 5;
+                if (nx < cx) {
+                    res.append(cx - nx, 'U');
+                }
+                if (ny < cy) {
+                    res.append(cy - ny, 'L');
+                }
+                if (nx > cx) {
+                    res.append(nx - cx, 'D');
+                }
+                if (ny > cy) {
+                    res.append(ny - cy, 'R');
+                }
+                res.push_back('!');
+                cx = nx;
+                cy = ny;
+            }
+            return res;
+    }
 };
 #endif //LEARNCPP_ALGORITHM_SOLUTION2_H
