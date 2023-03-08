9e66f0f21656fe55c58050c9f284c5afd5d96852
Bao <aolei.bao@accenture.com>
2023-03-08 11:41:21 +0800

    modify solution2

--- leetcode/daily/Solution2.h
+++ leetcode/daily/Solution2.h
@@ -4,6 +4,7 @@
 
 #ifndef LEARNCPP_ALGORITHM_SOLUTION2_H
 #define LEARNCPP_ALGORITHM_SOLUTION2_H
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -14,7 +15,7 @@ using namespace std;
 
 class Solution2 {
 public:
-    vector<string> alertNames(vector<string>& keyName, vector<string>& keyTime) {
+    vector<string> alertNames(vector<string> &keyName, vector<string> &keyTime) {
         unordered_map<string, vector<int>> timeMap;
         int n = keyName.size();
         for (int i = 0; i < n; i++) {
@@ -25,7 +26,7 @@ public:
             timeMap[name].emplace_back(hour * 60 + minute);
         }
         vector<string> res;
-        for (auto &[name, list] : timeMap) {
+        for (auto &[name, list]: timeMap) {
             sort(list.begin(), list.end());
             int size = list.size();
             for (int i = 2; i < size; i++) {
@@ -41,19 +42,19 @@ public:
         return res;
     }
 
-    vector<string> removeSubfolders(vector<string>& folder) {
+    vector<string> removeSubfolders(vector<string> &folder) {
         sort(folder.begin(), folder.end());
         string cur = folder[0];
         vector<string> ans;
         ans.push_back(cur);
         for (int i = 1; i < folder.size(); ++i) {
-            if (cur.size()<= 1){
+            if (cur.size() <= 1) {
                 cur = folder[i];
                 ans.push_back(cur);
-            }else{
-                if (folder[i].substr(0, cur.size()) == cur && folder[i][cur.size()] == '/'){
+            } else {
+                if (folder[i].substr(0, cur.size()) == cur && folder[i][cur.size()] == '/') {
                     continue;
-                }else{
+                } else {
                     cur = folder[i];
                     ans.push_back(cur);
                 }
@@ -64,7 +65,7 @@ public:
 
     string printBin(double num) {
         string res = "0.";
-        while (res.size() <= 32 && num!= 0){
+        while (res.size() <= 32 && num != 0) {
             num *= 2;
             int digit = num;
             res.push_back(digit + '0');
@@ -73,25 +74,66 @@ public:
         return res.size() <= 32 ? res : "ERROR";
     }
 
-    vector<string> getFolderNames(vector<string>& names) {
-        unordered_map<string, int>nameCount;
+    vector<string> getFolderNames(vector<string> &names) {
+        unordered_map<string, int> nameCount;
         vector<string> res;
-        for (string & name: names) {
+        for (string &name: names) {
             auto it = nameCount.find(name);
-            if(it == nameCount.end()){
+            if (it == nameCount.end()) {
                 res.push_back(name);
                 nameCount[name] = 1;
-            }else{
+            } else {
                 string newName = name + "(" + to_string(it->second) + ")";
-                while (nameCount.find(newName) != nameCount.end()){
+                while (nameCount.find(newName) != nameCount.end()) {
                     it->second++;
                     newName = name + "(" + to_string(it->second) + ")";
                 }
                 res.push_back(newName);
-                it -> second ++;
+                it->second++;
             }
         }
         return res;
     }
+
+    int minimumDeletions(string s) {
+        vector<pair<int, int>> pairCountForward;
+        vector<pair<int, int>> pairCountBackward;
+        int curACount = 0, curBCount = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == 'a') {
+                curACount++;
+            } else {
+                curBCount++;
+            }
+            pairCountForward.emplace_back(curACount, curBCount);
+        }
+        curACount = 0;
+        curBCount = 0;
+        for (int i = s.size() - 1; i >= 0; --i) {
+            if (s[i] == 'a') {
+                curACount++;
+            } else {
+                curBCount++;
+            }
+            pairCountBackward.emplace_back(curACount, curBCount);
+        }
+        int ans = INT_MAX;
+        for (int i = 0; i < pairCountForward.size(); ++i) {
+            int forward = 0;
+            if (i > 0) {
+                forward = pairCountForward[i - 1].second;
+            }
+            int backward = 0;
+            if (i < pairCountForward.size() - 1) {
+                backward = pairCountBackward[pairCountForward.size() - 2 - i].first;
+            }
+            if (forward + backward < ans) {
+                ans = forward + backward;
+            }
+        }
+
+        return ans;
+    }
 };
+
 #endif //LEARNCPP_ALGORITHM_SOLUTION2_H
--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@ using namespace std;
 int main() {
     vector<string> names({"gta","gta(1)","gta","avalon"});
     Solution2 solution2;
-    for (string &s:solution2.getFolderNames(names)) {
-        cout << s << endl;
-    }
+    cout<<solution2.minimumDeletions("bbaaaaabb");
     return 0;
 }
